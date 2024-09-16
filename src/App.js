d7cbedcab078ad8c9532748ab9d3734863beb9eb
Praveen Kumar Upadhyay <71806052+praveenupadhyay997@users.noreply.github.com>
2024-09-16 08:12:53 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import "./fontstyles.css";
+import AppWelcome from "./pages/AppWelcome";
+
+export default function App() {
+  return (
+    <div className="App">
+      <AppWelcome />
+    </div>
+  );
+}
---
+++ src/pages/AppWelcome.js
@@ -0,0 +1,7 @@
+import "./AppWelcome.css";
+
+const AppWelcome = () => {
+  return <h1 className="welcome-card roboto-bold">Opinions</h1>;
+};
+
+export default AppWelcome;
